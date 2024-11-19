329fb0c2d84d48329e41548db0a074482e3bfc6e
worsleyj <worsleyj23@gmail.com>
2024-11-19 14:40:41 -0500

    add POST, fix dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
@@ -27,6 +28,18 @@ app.get("/", (req, res) => {
 
 app.get("/sign-up", (req, res) => {
   res.render("sign-up-form");
+});
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
 });
 
 app.listen(3000, () =>
