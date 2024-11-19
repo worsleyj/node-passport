93b92fa7bcc29585d03dfdabe98a108372e10445
worsleyj <worsleyj23@gmail.com>
2024-11-19 14:22:25 -0500

    add app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("rexpress");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const pool = new Pool({
+  host: "localhost",
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: "passport",
+  port: 5432,
+});
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: fales }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () =>
+  console.log("passport application launched on port 3000")
+);
