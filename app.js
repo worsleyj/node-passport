40b511cf114e150380115d73d19f8a3bbbad8c35
worsleyj <worsleyj23@gmail.com>
2024-11-19 14:24:12 -0500

    add index view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require("node:path");
 const { Pool } = require("pg");
-const express = require("rexpress");
+const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
-app.use(express.urlencoded({ extended: fales }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
